900f285d25200c7e3d3464eceeb91b865262004f
sabrina010209 <mcpe11640@gmail.com>
2024-08-03 08:11:59 -0500

    Import A72 defconfig and re do the build script

---
+++ build_a72.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCH=arm64
+export KBUILD_BUILD_HOST="ubuntu"
+export KBUILD_BUILD_USER="seb"
+
+make O=out ARCH=arm64 vendor/a72q_eur_open_defconfig
+
+
+PATH="${PWD}/toolchains/gcc_aarch64/bin:${PWD}/toolchains/gcc_arm/bin:${PATH}:${PWD}/toolchains/clang/bin:${PATH}"
+ccache make O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu-
