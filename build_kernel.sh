225b8385d2c50f521feae9ac2f9c7215ba1ea8f0
sabrina010209 <mcpe11640@gmail.com>
2024-08-02 21:53:16 -0500

    Rework build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 export ARCH=arm64
-mkdir out
+export KBUILD_BUILD_HOST="ubuntu"
+export KBUILD_BUILD_USER="seb"
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+make O=out ARCH=arm64 vendor/a72q_eur_open_defconfig
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/a72q_eur_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
- 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+
+PATH="${PWD}/toolchains/gcc_aarch64/bin:${PWD}/toolchains/gcc_arm/bin:${PATH}:${PWD}/toolchains/clang/bin:${PATH}"
+ccache make O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu-
